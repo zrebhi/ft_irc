89e502bc6c55936f2e099acd5a26a70f76f9e258
baptiste gresse <Baptiste5gresse@gmail.com>
2023-10-17 12:32:15 +0200

    fixed error merge on header command

--- .obj/server/ServerUtils.d
+++
@@ -1,9 +0,0 @@
-.obj/server/ServerUtils.o: srcs/server/ServerUtils.cpp \
- srcs/server/Server.hpp srcs/server/../client/Client.hpp \
- srcs/server/../channels/Channel.hpp srcs/server/../commands/Command.hpp \
- srcs/server/../utils/Utils.hpp
-srcs/server/Server.hpp:
-srcs/server/../client/Client.hpp:
-srcs/server/../channels/Channel.hpp:
-srcs/server/../commands/Command.hpp:
-srcs/server/../utils/Utils.hpp:
